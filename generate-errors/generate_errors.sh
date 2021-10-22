eab8db14b30ae2ac41af20da065434cac637cc39
Niklas Eicker <git@nikl.me>
2021-10-22 11:00:22 +0200

    Render all error content

--- generate-errors/generate_errors.sh
+++ generate-errors/generate_errors.sh
@@ -2,9 +2,9 @@
 
 git init errors
 cd errors
-git remote add origin https://github.com/bevyengine/bevy
+git remote add origin https://github.com/mockersf/bevy
 git sparse-checkout set "errors"
-git pull --depth=1 origin main
+git pull --depth=1 origin error-codes
 cd ..
 
 cargo run --bin generate -- errors ../content
