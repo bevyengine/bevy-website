663659928128b7cf9fdfc8adceb5749631da2888
Bill Johnson <wrjstuff@gmail.com>
2022-04-05 17:36:08 -0400

    Rustup is faster for some weird reason.

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-amazon-linux-extras install rust1
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-# source $HOME/.cargo/env
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
 
 cd generate-assets
 ./generate_assets.sh
