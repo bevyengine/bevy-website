c0c2d28900b5e637551d2d135153fe278fc2c4e9
Bill Johnson <wrjstuff@gmail.com>
2022-04-05 17:22:53 -0400

    Make work

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 cd generate-assets
 ./generate_assets.sh
