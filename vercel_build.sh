c466b57616c16e7e60d25835e3c893ba61e1bf74
Bill Johnson <wrjstuff@gmail.com>
2022-04-05 17:22:10 -0400

    Accept defaults

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
 
 cd generate-assets
 ./generate_assets.sh
