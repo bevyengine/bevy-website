19c0575e82c8a19828101e1d000577cdcfac2ebc
Bill Johnson <wrjstuff@gmail.com>
2022-04-05 17:33:03 -0400

    Try built-in rust install

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+amazon-linux-extras install rust1
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# source $HOME/.cargo/env
 
 cd generate-assets
 ./generate_assets.sh
