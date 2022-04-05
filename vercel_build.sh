524b2c1058096844b2d2b357f613221ec615f258
Bill Johnson <wrjstuff@gmail.com>
2022-04-05 18:10:46 -0400

    Add wasm rust target.

--- vercel_build.sh
+++ vercel_build.sh
@@ -3,6 +3,8 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
+rustup target add wasm32-unknown-unknown
+
 cd generate-assets
 ./generate_assets.sh
 cd ..
