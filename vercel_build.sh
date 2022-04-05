aa0c4dbc0bf49e3b2dddd72306c26c6fe14a92c8
Bill Johnson <wrjstuff@gmail.com>
2022-04-05 19:11:43 -0400

    Check to make sure normal builds work.

--- vercel_build.sh
+++ vercel_build.sh
@@ -3,7 +3,8 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-rustup target add wasm32-unknown-unknown
+# TODO smarter wasm-enabled builds
+# rustup target add wasm32-unknown-unknown
 
 cd generate-assets
 ./generate_assets.sh
@@ -13,9 +14,10 @@ cd generate-errors
 ./generate_errors.sh
 cd ..
 
+# TODO smarter wasm-enabled builds
 # cargo install wasm-bindgen-cli
 cd generate-wasm-examples
-SKIP_WASM=true ./generate_wasm_examples.sh
+./generate_wasm_examples.sh
 cd ..
 
 zola build
