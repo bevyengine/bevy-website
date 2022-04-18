bf879ec73f12a33397c9246d1438ef5b8d0a3844
doup <asier@illarra.com>
2022-04-18 09:58:30 +0200

    Use category/example slugs for generated example content

--- generate-wasm-examples/generate_wasm_examples.sh
+++ generate-wasm-examples/generate_wasm_examples.sh
@@ -19,9 +19,10 @@ add_category()
 {
     category=$1
     category_path=$2
+    category_slug=`echo $category_path | tr '_' '-'`
     example_weight=0
 
-    mkdir ../../content/examples/$category_path
+    mkdir ../../content/examples/$category_slug
 
     # Remove first two arguments
     shift 2
@@ -31,18 +32,21 @@ add_category()
     for example in $@
     do
         echo "building $category / $example"
-        mkdir ../../content/examples/$category_path/$example
-        cp examples/$category_path/$example.rs ../../content/examples/$category_path/$example/
+        example_slug=`echo $example | tr '_' '-'`
+        code_filename="$example.rs"
+        mkdir ../../content/examples/$category_slug/$example_slug
+        cp examples/$category_path/$code_filename ../../content/examples/$category_slug/$example_slug/
         cargo build --release --target wasm32-unknown-unknown --example $example
-        wasm-bindgen --out-dir ../../content/examples/$category_path/$example --no-typescript --target web target/wasm32-unknown-unknown/release/examples/$example.wasm
+        wasm-bindgen --out-dir ../../content/examples/$category_slug/$example_slug --no-typescript --target web target/wasm32-unknown-unknown/release/examples/$example.wasm
         echo "+++
 title = \"$example\"
 template = \"example.html\"
 weight = $example_weight
 
 [extra]
+code_filename = \"$code_filename\"
 header_message = \"Examples\"
-+++" > ../../content/examples/$category_path/$example/index.md
++++" > ../../content/examples/$category_slug/$example_slug/index.md
 
         example_weight=$((example_weight+1))
     done
@@ -52,7 +56,10 @@ header_message = \"Examples\"
 title = \"$category\"
 sort_by = \"weight\"
 weight = $category_weight
-+++" > ../../content/examples/$category_path/_index.md
+
+[extra]
+code_folder = \"$category_path\"
++++" > ../../content/examples/$category_slug/_index.md
 
     category_weight=$((category_weight+1))
 }
