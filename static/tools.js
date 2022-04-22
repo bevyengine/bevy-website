f0a4c21099ccf08e5f4a49257df23b418ddd63a8
Asier Illarramendi <asier@illarra.com>
2022-04-22 09:39:39 +0200

    Show filename instead of showing multiple progress bars

--- static/tools.js
+++ static/tools.js
@@ -1,31 +1,44 @@
 import { ReadableStream as PolyfillReadableStream, TransformStream as PolyfillTransformStream } from '/web-streams.es6.mjs';
 import { createReadableStreamWrapper } from '/web-streams-adapter.mjs';
 
+function getFilename(resource) {
+    const pathname = (typeof resource === 'string')
+        ? resource
+        : (resource instanceof URL)
+            ? resource.pathname
+            : '';
+
+    const parts = pathname.split('/');
+
+    return parts[parts.length - 1];
+}
+
 // `progressiveFetch` is a wrapper over `window.fetch`. It allows you to insert middle-ware that is
 // polled as the fetch completes. See bevy-website/issues/338 for details.
 async function progressiveFetch(resource, callbacks={}) {
   const toPolyfillReadable = createReadableStreamWrapper(PolyfillReadableStream);
   const toNativeReadable = createReadableStreamWrapper(window.ReadableStream);
+  const filename = getFilename(resource);
 
   const cb = Object.assign({
-    start: (length) => {},
-    update: (loaded, length) => {},
-    finish: (length) => {},
+    start: (filename, length) => {},
+    update: (filename, loaded, length) => {},
+    finish: (filename, length) => {},
   }, callbacks);
   let response = await fetch(resource);
   const lengthBytes = response.headers.get('content-length');
   let loadedBytes = 0;
   const transform = new PolyfillTransformStream({
     start() {
-      cb.start(lengthBytes);
+      cb.start(filename, lengthBytes);
     },
     transform(chunk, controller) {
       loadedBytes += chunk.byteLength;
-      cb.update(loadedBytes, lengthBytes);
+      cb.update(filename, loadedBytes, lengthBytes);
       controller.enqueue(chunk);
     },
     flush() {
-      cb.finish(lengthBytes);
+      cb.finish(filename, lengthBytes);
     },
   });
   return new Response(toNativeReadable(toPolyfillReadable(response.body).pipeThrough(transform)), response);
