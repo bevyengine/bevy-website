df9e69e4c53ba8d69922cb5ef2637881f03b859d
IceSentry <c.giguere42@gmail.com>
2022-10-25 17:07:02 -0400

    generate release note

--- generate-migration-guide/src/main.rs
+++ generate-migration-guide/src/main.rs
@@ -63,45 +63,56 @@ fn main() -> anyhow::Result<()> {
             &date,
             path.unwrap_or(PathBuf::from("./migration-guide.md")),
         )?,
-        Commands::ReleaseNote { date, path } => todo!(),
+        Commands::ReleaseNote { date, path } => {
+            generate_release_note(&date, path.unwrap_or(PathBuf::from("./release-note.md")))?
+        }
     };
 
     Ok(())
 }
 
-#[derive(Deserialize, Clone)]
-struct GithubIssuesResponse {
-    title: String,
-    number: i32,
-    body: String,
-}
+fn generate_release_note(date: &str, path: PathBuf) -> anyhow::Result<()> {
+    let mut prs = Vec::<GithubIssuesResponse>::with_capacity(100);
+    let mut page = 1;
+    loop {
+        println!("Page: {}", page);
+        let mut prs_in_page = get_merged_prs(date, page, None)?;
+        if prs_in_page.is_empty() {
+            break;
+        } else {
+            page += 1;
+        }
 
-/// Get all closed PRs with the label `C-Breaking-Change` since the given date
-fn get_breaking_prs(date: &str) -> anyhow::Result<Vec<GithubIssuesResponse>> {
-    let token = std::env::var("GITHUB_TOKEN")
-        .expect("GITHUB_TOKEN not found, github links will be skipped");
-    let agent: ureq::Agent = ureq::AgentBuilder::new()
-        .user_agent("bevy-website-generate-migration-guide")
-        .build();
-    let response: Vec<GithubIssuesResponse> = agent
-        .get(&format!(
-            "https://api.github.com/repos/bevyengine/bevy/issues"
-        ))
-        .set("Accept", "application/json")
-        .set("Authorization", &format!("Bearer {}", token))
-        .query("state", "closed")
-        .query("labels", "C-Breaking-Change")
-        // release date of 0.8, could probably be automated
-        .query("since", &format!("{}T00:00:00Z", date))
-        .query("per_page", "100")
-        .call()?
-        .into_json()?;
-    Ok(response
-        .iter()
-        // Make sure to only get the PRs that were merged by bors
-        .filter(|pr| pr.title.starts_with("[Merged by Bors] - "))
-        .cloned()
-        .collect())
+        prs.append(&mut prs_in_page);
+    }
+
+    let mut output = String::new();
+    writeln!(&mut output, "# Full Changelog")?;
+    for pr in &prs {
+        let mut pr_title = pr
+            .title
+            .replace("[Merged by Bors] - ", "")
+            .trim()
+            .to_string();
+        writeln!(&mut output, "\n## {}\n", pr_title)?;
+        writeln!(
+            &mut output,
+            "Labels: {:?}",
+            pr.labels.iter().map(|l| l.name.clone()).collect::<Vec<_>>()
+        )?;
+        writeln!(&mut output, "Created by: {}", pr.user.login)?;
+        writeln!(
+            &mut output,
+            "link: <https://github.com/bevyengine/bevy/pull/{}>",
+            pr.number
+        )?;
+    }
+
+    println!("Found {} prs merged by bors since {}", prs.len(), date);
+
+    std::fs::write(path, output)?;
+
+    Ok(())
 }
 
 fn generate_migration_guide(
@@ -129,23 +140,27 @@ long_title = "Migration Guide: {}"
     )?;
     writeln!(&mut output)?;
 
-    let prs = get_breaking_prs(&date)?;
+    let prs = get_merged_prs(&date, 0, Some("C-Breaking-Change"))?;
     for pr in &prs {
-        println!("## {}", pr.title.replace("[Merged by Bors] - ", "").trim());
+        let mut pr_title = pr
+            .title
+            .replace("[Merged by Bors] - ", "")
+            .trim()
+            .to_string();
+        println!("# {}", pr_title);
 
         // Write title for the PR with correct heading and github url
         writeln!(
             &mut output,
             "\n### [{}](https://github.com/bevyengine/bevy/pull/{})",
-            pr.title.replace("[Merged by Bors] - ", "").trim(),
-            pr.number
+            pr_title, pr.number
         )?;
 
         // Parse the body of the PR
         let mut options = Options::empty();
         options.insert(Options::ENABLE_TABLES);
         options.insert(Options::ENABLE_SMART_PUNCTUATION);
-        let mut markdown = Parser::new_ext(&pr.body, options);
+        let mut markdown = Parser::new_ext(&pr.body.as_ref().unwrap(), options);
         let mut guide_found = false;
 
         while let Some(event) = markdown.next() {
@@ -208,9 +223,61 @@ long_title = "Migration Guide: {}"
         }
     }
 
-    std::fs::write(path, output)?;
-
     println!("\nFound {} breaking PRs merged by bors", prs.len());
 
+    std::fs::write(path, output)?;
+
     Ok(())
+}
+
+#[derive(Deserialize, Clone, Debug)]
+struct GithubIssuesResponse {
+    title: String,
+    number: i32,
+    body: Option<String>,
+    labels: Vec<GithubLabel>,
+    user: GithubUser,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+struct GithubLabel {
+    name: String,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+struct GithubUser {
+    login: String,
+}
+
+// TODO handle pages
+fn get_merged_prs(
+    date: &str,
+    page: i32,
+    label: Option<&str>,
+) -> anyhow::Result<Vec<GithubIssuesResponse>> {
+    let token = std::env::var("GITHUB_TOKEN")
+        .expect("GITHUB_TOKEN not found, github links will be skipped");
+    let agent: ureq::Agent = ureq::AgentBuilder::new()
+        .user_agent("bevy-website-generate-migration-guide")
+        .build();
+    let mut request = agent
+        .get(&format!(
+            "https://api.github.com/repos/bevyengine/bevy/issues"
+        ))
+        .set("Accept", "application/json")
+        .set("Authorization", &format!("Bearer {}", token))
+        .query("state", "closed")
+        .query("since", &format!("{}T00:00:00Z", date))
+        .query("per_page", "100")
+        .query("page", &page.to_string());
+    if let Some(label) = label {
+        request = request.query("labels", label);
+    }
+    let response: Vec<GithubIssuesResponse> = request.call()?.into_json()?;
+    Ok(response
+        .iter()
+        // Make sure to only get the PRs that were merged by bors
+        .filter(|pr| pr.title.starts_with("[Merged by Bors] - "))
+        .cloned()
+        .collect())
 }
