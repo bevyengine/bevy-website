cc9c1c2761e77385ff18938fe0ccceb4be449bab
François <mockersf@gmail.com>
2022-12-19 12:25:22 +0100

    use main branch

--- generate-community/generate_community.sh
+++ generate-community/generate_community.sh
@@ -3,7 +3,7 @@
 git init bevy-community
 cd bevy-community
 git remote add origin https://github.com/bevyengine/bevy-community
-git pull --depth=1 origin latest
+git pull --depth=1 origin main
 cd ..
 
 cargo run --bin generate -- bevy-community ../content/ community
