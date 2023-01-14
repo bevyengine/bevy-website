859332263efc781d2ccfef200cc04937ebc3131e
Carter Anderson <mcanders1@gmail.com>
2023-01-14 14:11:28 -0800

    Use upstream bevy-community

--- generate-community/generate_community.sh
+++ generate-community/generate_community.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/cart/bevy-community
+git init bevy-community
 cd bevy-community
-git checkout roles-and-unify
-# git init bevy-community
-# cd bevy-community
-# git remote add origin https://github.com/bevyengine/bevy-community
-# git pull --depth=1 origin main
+git remote add origin https://github.com/bevyengine/bevy-community
+git pull --depth=1 origin main
 cd ..
 
 cargo run --bin generate -- bevy-community ../content/ community
