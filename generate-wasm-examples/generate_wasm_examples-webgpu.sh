f4c1a0c3accbcc8131d4d2b7e43bd262daa0b54f
François <mockersf@gmail.com>
2023-05-06 21:53:08 +0200

    build webgpu example page

---
+++ generate-wasm-examples/generate_wasm_examples-webgpu.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git init bevy-main
+cd bevy-main
+git remote add origin https://github.com/mockersf/bevy
+git pull --depth=1 origin example-runner
+
+cd bevy-main
+
+cargo run -p example-showcase -- build-website-list --content-folder content
+mv content ../../content/examples-webgpu
