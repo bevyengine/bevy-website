6ccb493a82a7538cc15cc5e3a10913c30ca0eb90
François <mockersf@gmail.com>
2023-05-07 22:12:28 +0200

    don't double go to the folder
    Co-authored-by: Paul Hansen <mail@paul.rs>

--- generate-wasm-examples/generate_wasm_examples-webgpu.sh
+++ generate-wasm-examples/generate_wasm_examples-webgpu.sh
@@ -5,7 +5,5 @@ cd bevy-main
 git remote add origin https://github.com/mockersf/bevy
 git pull --depth=1 origin example-runner
 
-cd bevy-main
-
 cargo run -p example-showcase -- build-website-list --content-folder content
 mv content ../../content/examples-webgpu
