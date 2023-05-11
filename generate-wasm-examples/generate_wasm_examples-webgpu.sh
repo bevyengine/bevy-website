498eab341b38d2fc18f4f4d4a76060621ab7948b
François <mockersf@gmail.com>
2023-05-12 00:57:05 +0200

    showcase tool has been merged in Bevy main

--- generate-wasm-examples/generate_wasm_examples-webgpu.sh
+++ generate-wasm-examples/generate_wasm_examples-webgpu.sh
@@ -2,8 +2,8 @@
 
 git init bevy-main
 cd bevy-main
-git remote add origin https://github.com/mockersf/bevy
-git pull --depth=1 origin example-runner
+git remote add origin https://github.com/bevyengine/bevy
+git pull --depth=1 origin main
 
 cargo run -p example-showcase -- build-website-list --content-folder content
 mv content ../../content/examples-webgpu
