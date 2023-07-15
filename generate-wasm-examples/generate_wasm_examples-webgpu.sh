badbd0d947b35d0e6b8e164f82d92f8fb4fc0d3a
François <mockersf@gmail.com>
2023-07-15 16:02:42 +0200

    Build examples in CI and update WebGL2 examples

--- generate-wasm-examples/generate_wasm_examples-webgpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git init bevy-main
-cd bevy-main
-git remote add origin https://github.com/bevyengine/bevy
-git pull --depth=1 origin main
-
-cargo run -p example-showcase -- build-website-list --content-folder content
-mv content ../../content/examples-webgpu
