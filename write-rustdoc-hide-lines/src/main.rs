24680b8b65665aa92943c5e89035a3762ceee8ed
BD103 <59022059+BD103@users.noreply.github.com>
2024-02-05 22:01:51 -0500

    feat: allow passing multiple folders to write-rustdoc-hide-lines
    Additionally write some usage documentation and make the program return an exit code if it errors.

--- write-rustdoc-hide-lines/src/main.rs
+++ write-rustdoc-hide-lines/src/main.rs
@@ -1,16 +1,50 @@
-use std::{env, path::Path};
-
+use std::{env, path::PathBuf, process::ExitCode};
 use write_rustdoc_hide_lines::formatter;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let folder = args.get(1).expect("Please provide a folder");
-    let path = Path::new(&folder);
+/// Generates `hide_lines` annotations to Rust code blocks.
+///
+/// In most cases you can just call `write_rustdoc_hide_lines.sh`, which will automatically handle
+/// formatting all files.
+///
+/// ```shell
+/// $ cd write-rustdoc-hide-lines
+/// $ ./write_rustdoc_hide_lines.sh
+/// ```
+///
+/// You can also run the executable manually.
+///
+/// ```shell
+/// $ cd write-rustdoc-hide-lines
+///
+/// # Format one folder.
+/// $ cargo run -- ../content/learn/book
+///
+/// # Format multiple folders.
+/// $ cargo run -- ../content/learn/book ../content/learn/quick-start
+/// ```
+fn main() -> ExitCode {
+    // The first argument is usually the executable path, so we skip that to just get arguments.
+    let folders = env::args().skip(1).map(PathBuf::from);
 
-    println!("Formatting folder: {:?}", path);
+    if folders.len() == 0 {
+        eprintln!("Did not format any files because no folder argument was passed.");
 
-    match formatter::run(path) {
-        Ok(_) => println!("Done!"),
-        Err(error) => println!("Error: {}", error),
+        return ExitCode::FAILURE;
     }
+
+    for folder in folders {
+        println!("Formatting folder: {:?}", folder);
+
+        // Format the given path, printing out errors as they occur.
+        if let Err(error) = formatter::run(&folder) {
+            eprintln!("Error: {}", error);
+
+            // Exit early if an error occurred.
+            return ExitCode::FAILURE;
+        }
+    }
+
+    println!("Done!");
+
+    return ExitCode::SUCCESS;
 }
