840f0da19715da9073974d7feb0aea864df4cd88
BD103 <59022059+BD103@users.noreply.github.com>
2024-02-15 21:43:28 -0500

    chore: add comments to shell scripts

--- generate-errors/generate_errors.sh
+++ generate-errors/generate_errors.sh
@@ -3,6 +3,7 @@
 # Switch to script's directory, letting it be called from any folder.
 cd $(dirname $0)
 
+# Only download the `errors` folder from the main Bevy repository.
 git init bevy
 cd bevy
 git remote add origin https://github.com/bevyengine/bevy
--- generate-wasm-examples/clone_bevy.sh
+++ generate-wasm-examples/clone_bevy.sh
@@ -3,6 +3,7 @@
 # Switch to script's directory, letting it be called from any folder.
 cd $(dirname $0)
 
+# FIXME: Can this be shortened to git clone --depth=1?
 git init bevy
 cd bevy
 git remote add origin https://github.com/bevyengine/bevy
