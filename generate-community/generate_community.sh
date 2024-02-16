d17be0dafb1b322a0c2a4ca0029ed65864b2fa66
BD103 <59022059+BD103@users.noreply.github.com>
2024-02-16 12:53:11 -0500

    Simplify shallow clones in shell scripts (#967)

--- generate-community/generate_community.sh
+++ generate-community/generate_community.sh
@@ -4,11 +4,6 @@
 cd $(dirname $0)
 
 # Download a copy of the Bevy community repository.
-# FIXME: Can this be shortened to git clone --depth=1?
-git init bevy-community
-cd bevy-community
-git remote add origin https://github.com/bevyengine/bevy-community
-git pull --depth=1 origin main
-cd ..
+git clone --depth=1 https://github.com/bevyengine/bevy-community bevy-community
 
 cargo run --bin generate -- bevy-community ../content/ community
