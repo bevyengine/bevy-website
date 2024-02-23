7c17bb19eab6db58f5d60e1f413da9c991a9bf66
BD103 <59022059+BD103@users.noreply.github.com>
2024-02-22 22:54:07 -0500

    chore: improve `generate_wasm_examples.sh`
    - Adds more logging about what the script is doing.
    - Adds comments and update existing ones.
    - Deletes `clone_bevy.sh`, merging its contents into the main script.
    - Fetches latest commit if Bevy was already cloned.
    - Deletes built example folders if they already exist.
    - Removes temporary main branch tools patch.
    - Removes unecessary `mv content` call because `cargo run` supports `--content-folder`.

--- generate-wasm-examples/generate_wasm_examples.sh
+++ generate-wasm-examples/generate_wasm_examples.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 
+echo Generating WASM example list...
+
 # Switch to script's directory, letting it be called from any folder.
 cd $(dirname $0)
 
-./clone_bevy.sh
+# If Bevy folder already exists, pull the latest changes.
+if [[ -d bevy ]]; then
+    echo Bevy folder already exists, attempting to fetch latest changes.
 
-# temporary: fetch tools from main branch
-git clone --depth=1 https://github.com/bevyengine/bevy bevy-tools
+    cd bevy
 
-rm -rf bevy/tools
-cp -r bevy-tools/tools bevy
-rm -rf bevy-tools
-cd bevy
+    # Attempts to fetch the latest commits, which should only happen every Bevy release.
+    git pull --depth=1
+else
+    echo Bevy folder does not exist, cloning repository.
 
-cargo run -p example-showcase -- build-website-list --content-folder content --api webgl2
-mv content ../../content/examples
+    # Clone Bevy's latest branch from scratch, only downloading the latest commit.
+    git clone --depth=1 --branch=latest https://github.com/bevyengine/bevy bevy
 
-rm -rf content
+    cd bevy
+fi
 
-cargo run -p example-showcase -- build-website-list --content-folder content --api webgpu
-mv content ../../content/examples-webgpu
+# Build WebGL2 example list.
+rm -rf ../../content/examples
+cargo run -p example-showcase -- build-website-list --content-folder ../../content/examples --api webgl2
 
-# remove markdown files from assets so that they don't get picked up by Zola
+# Build WebGPU example list.
+rm -rf ../../content/examples-webgpu
+cargo run -p example-showcase -- build-website-list --content-folder ../../content/examples-webgpu --api webgpu
+
+# Remove Markdown files from assets so that they don't get picked up by Zola.
 find assets -type f -name '*.md' -exec rm {} +
+
+# Copy remaining assets to examples folder.
 cp -r assets/ ../../static/assets/examples/
+
+echo Finished generating WASM example list! \(WebGL2 + WebGPU\)
