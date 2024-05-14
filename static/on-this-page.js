39571d8737e990bba751066096717ae1a8e7f9fb
SIGSTACKFAULT <sigstackfault@gmail.com>
2024-05-14 17:57:49 -0400

    rename all variables to start with `otp_`

--- static/on-this-page.js
+++ static/on-this-page.js
@@ -1,10 +1,10 @@
 //! in the page-level navigation, sets data-active=true on the li which you're currently looking at
 
-let page_nav_state = new Map();
+let otp_state = new Map();
 
 /// give data-active=true to the page nav link which points to the given id; set all others to false.
 /// accepts an id string or a DOM node which it will automatically retrieve the id of
-function page_nav_set_active(id_or_node){
+function otp_set_active(id_or_node){
   let id = "";
   if(typeof id_or_node == "object"){
     id = id_or_node.getAttribute("id");
@@ -16,21 +16,21 @@ function page_nav_set_active(id_or_node){
   });
 }
 
-let page_nav_observer =  new IntersectionObserver(
+let otp_observer =  new IntersectionObserver(
   entries => {
     entries.forEach(entry => {
-      page_nav_state.set(entry.target, entry.isIntersecting);
+      otp_state.set(entry.target, entry.isIntersecting);
     });
-    let intersecting = Array.from(page_nav_state).filter(([k,v]) => v == true);
+    let intersecting = Array.from(otp_state).filter(([k,v]) => v == true);
     intersecting.sort(([k,v]) => k.clientTop)
     if (intersecting.length > 0) {
-      page_nav_set_active(intersecting[0][0]);
+      otp_set_active(intersecting[0][0]);
     }
   }, {
     rootMargin: "0px 0px 20% 0px",
     threshold: 1.0,
   });
 
-page_nav_set_active(document.querySelector("main h2"));
+otp_set_active(document.querySelector("main h2"));
 document.querySelectorAll("main h2, main h3")
-  .forEach(h => page_nav_observer.observe(h));
+  .forEach(h => otp_observer.observe(h));
