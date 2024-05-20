cbd6ef3b5451babc584383eca75adca3c1f1a8c4
SIGSTACKFAULT <sigstackfault@gmail.com>
2024-05-20 17:17:37 -0400

    rewrite time already
    declare search history out of scope

--- static/search.js
+++ static/search.js
@@ -1,111 +1,33 @@
-const SEARCH_HISTORY_LOCALSTORAGE_KEY = "search_history"
-const SEARCH_HISTORY_MAX = 5;
-/** 
- * <script> tags are ordered such that this will work without checking
- * types stolen from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/elasticlunr/index.d.ts
- * @typedef {{score: number, ref: string, doc: {title: string, body: string}}} SearchResult
- * @type {{search: (query: string) => SearchResult[]}}
- */
-const SEARCH_INDEX = elasticlunr.Index.load(searchIndex);
-
-/**
- * opens the search dialog, updates "recent searches", and randomizes the dialog tip
- */
-function search_open() {
-    document.getElementById("search-dialog").showModal();
-    search_history_show();
-    search_tip();
-    search_key_handler();
-}
-
-/**
- * get history from localStorage
- * clears history and returns [] if anything goes wrong
- * @returns {string[]}
- */
-function search_history_get() {
-    let history = localStorage.getItem(SEARCH_HISTORY_LOCALSTORAGE_KEY) ?? "[]";
-    try {
-        history = JSON.parse(history);
-    } catch {
-        console.warn(`Expected localStorage ${SEARCH_HISTORY_LOCALSTORAGE_KEY} to parse as json, but it was`, history, "clearing.");
-        localStorage.removeItem(SEARCH_HISTORY_LOCALSTORAGE_KEY);
-        return [];
-    }
-    console.trace("history from localStorage:", history)
-    if (!Array.isArray(history)) {
-        console.warn(`Expected localStorage ${SEARCH_HISTORY_LOCALSTORAGE_KEY} to be an array, but it was`, history, "clearing.")
-        localStorage.removeItem(SEARCH_HISTORY_LOCALSTORAGE_KEY);
-        return [];
-    }
-    return history;
-}
-
-/**
- * add `search` to history in localStorage. does not update #search-dialog. returns the new history array.
- * @param {string} search 
- * @returns {string[]}
- */
-function search_history_push(search) {
-    let history = search_history_get();
-    history.splice(0, 0, search);
-    if (history.length > SEARCH_HISTORY_MAX) {
-        history.pop()
-    }
-    localStorage.setItem(SEARCH_HISTORY_LOCALSTORAGE_KEY, JSON.stringify(history));
-    return history
-}
-
-/**
- * gets search history from localStorage and updates the list in #search-dialog
- */
-function search_history_show() {
-    let history = search_history_get();
-    let $parent = document.getElementById("search-dialog").querySelector(".search-dialog__recent");
-    let $none = $parent.querySelector(".search-dialog__recent__none");
-    let $some = $parent.querySelector(".search-dialog__recent__some");
-    let $list = $some.querySelector("ul");
-    if (history.length == 0) {
-        $some.style.display = "none";
-        $none.style.display = null;
-    } else {
-        $none.style.display = "none";
-        $some.style.display = null;
-        $list.innerHTML = ""; // remove children
-        history.forEach(item => {
-            let li = document.createElement("li");
-            li.innerText = item;
-            $list.appendChild(li)
-        });
-    }
-}
-
-/**
- * show a new search dialog tip
- */
-function search_tip() {
-    let $list = document.querySelector("#search-dialog aside ul");
-    let length = $list.children.length;
-    let choice = Math.floor(Math.random() * length);
-    console.trace("chose tip", choice);
-    Array.from($list.children).forEach((n, i) => n.setAttribute("data-chosen", i==choice));
-}
 
 /**
  * the actual sauce. sends the search off to elasticlunr and displays the results.
  */
-function search_key_handler(){
+function search_key_handler() {
+    console.trace("search_key_handler()")
     let $dialog = document.getElementById("search-dialog")
     let $recent = $dialog.querySelector(".search-dialog__recent")
     let $results = $dialog.querySelector(".search-dialog__results")
     let query = $dialog.querySelector("form input").value;
-    if(query.length > 0) {
+    if (query.length > 0) {
         $recent.style.display = "none";
+        $results.style.display = null;
     } else {
-        $recent.style.display = null;
+        $recent.style.display = "block";
+        $results.style.display = "none";
         return;
     }
-    let result = SEARCH_INDEX.search(query);
+    // search_history_push(query);
+    let result = SEARCH_INDEX.search(query, {
+        fields: {
+            title: { boost: 2 },
+            body: { boost: 1 }
+        },
+        expand: true
+    });
+    if (result.length == 0) {
+        console.trace("no results");
+    }
+    result = result.slice(0, SEARCH_RESULTS_LIMIT);
     $results.innerHTML = "";
     result.forEach(result => {
         let link = document.createElement("a");
@@ -124,3 +46,28 @@ document.getElementById("search-dialog").addEventListener('click', function (eve
         this.close();
     }
 });
+
+/// just a contrivance to clean up the global namespace
+class __Search {
+    RESULTS_LIMIT = 10;
+    $dialog = document.getElementById("search-dialog")
+    $search_tip_list = document.querySelector("#search-dialog aside ul");
+
+    open() {
+        this.$dialog.showModal();
+        this.change_tip();
+        // search_history_show();
+        // search_key_handler();
+    }
+
+    change_tip() {
+        let length = this.$search_tip_list.children.length;
+        let choice = Math.floor(Math.random() * length);
+        console.trace("chose tip", choice);
+        Array.from(this.$search_tip_list.children).forEach(
+            (tip, i) => tip.setAttribute("data-chosen", i == choice
+        ));
+    }
+}
+
+const SEARCH = new __Search()
