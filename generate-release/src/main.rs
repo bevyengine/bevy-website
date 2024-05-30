b645dabb09f1e14058076954b28059adf212fec2
Alice Cecile <alice.i.cecil@gmail.com>
2024-05-30 13:04:54 -0400

    Cargo fmt

--- generate-release/src/main.rs
+++ generate-release/src/main.rs
@@ -56,7 +56,7 @@ enum Commands {
         overwrite_existing: bool,
     },
     /// Generates release notes for all PRs merged with the `C-Needs-Release-Note` label.
-    /// 
+    ///
     /// This will also open an issue for each PR that doesn't have a release note.
     /// While duplicate issues will not be opened, be sure to use the `dry-run` flag
     /// if you want to test the release notes generation without spamming the repo.
