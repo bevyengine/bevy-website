a50e65759baf1d796750a7467192bca56f7066e5
TrialDragon <cj.2003@live.com>
2025-10-01 23:27:47 -0700

    Remove security sandbox, start last resort option

--- .pa11y-ci-dark.js
+++ .pa11y-ci-dark.js
@@ -4,7 +4,12 @@ const puppeteer = require("puppeteer");
 
 const getContext = async () => {
 	const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser'
+        executablePath: '/usr/bin/chromium-browser',
+        /* Disabling the sandbox is a bit dangerous and stupid,
+        but at this point we've ran out of options. The proper
+        way to solve this issue requires permissions on the
+        GitHub machine runner we can't have for security reasons. */
+        args: ['--no-sandbox']
     });
 	const page = await browser.newPage();
 	await page.emulateMediaFeatures([
