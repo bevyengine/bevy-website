58e83365f73ebbc3097e34fc24c8efca5aaac2b1
TrialDragon <cj.2003@live.com>
2025-10-02 15:34:22 -0700

    Revert "Remove security sandbox, start last resort option"
    This reverts commit a50e65759baf1d796750a7467192bca56f7066e5.

--- .pa11y-ci-dark.js
+++ .pa11y-ci-dark.js
@@ -4,12 +4,7 @@ const puppeteer = require("puppeteer");
 
 const getContext = async () => {
 	const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser',
-        /* Disabling the sandbox is a bit dangerous and stupid,
-        but at this point we've ran out of options. The proper
-        way to solve this issue requires permissions on the
-        GitHub machine runner we can't have for security reasons. */
-        args: ['--no-sandbox']
+        executablePath: '/usr/bin/chromium-browser'
     });
 	const page = await browser.newPage();
 	await page.emulateMediaFeatures([
